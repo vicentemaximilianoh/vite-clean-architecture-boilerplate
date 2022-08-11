6ace0250581bb16d1d85482f0f903b756dea98f0
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2022-08-11 15:19:31 -0300

    Added NotFound page

---
+++ src/presentation/pages/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+interface NotFoundPageProps {
+  
+}
+ 
+const NotFoundPage: React.FC<NotFoundPageProps> = () => {
+  return ( 
+    <section>
+      <h1>Not Found Page</h1>
+    </section>
+   );
+}
+ 
+export default NotFoundPage;
