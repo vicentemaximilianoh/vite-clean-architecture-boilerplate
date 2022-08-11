fcd5afec7e4c2ba6d04af9889342ee23369347a3
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2022-08-11 15:50:43 -0300

    Added eslint and prettier

--- src/presentation/pages/NotFoundPage.tsx
+++ src/presentation/pages/NotFoundPage.tsx
@@ -1,13 +1,11 @@
-interface NotFoundPageProps {
-  
-}
- 
+interface NotFoundPageProps {}
+
 const NotFoundPage: React.FC<NotFoundPageProps> = () => {
-  return ( 
+  return (
     <section>
       <h1>Not Found Page</h1>
     </section>
-   );
-}
- 
+  );
+};
+
 export default NotFoundPage;
